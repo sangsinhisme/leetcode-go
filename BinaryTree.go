4c0ba8ce841ef88bdb90dd0d295dbb7c183afe78
Nguyen Sang Sinh <sangsinh789@gmail.com>
2024-08-26 09:57:51 +0700

    add(day26, dfs): postorder tree

--- BinaryTree.go
+++ BinaryTree.go
@@ -8,9 +8,32 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
 func postorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return nil
 	}
 	return append(slices.Concat(postorderTraversal(root.Left), postorderTraversal(root.Right)), root.Val)
+}
+
+func dfs(node *Node, post *[]int) {
+	if node == nil {
+		return
+	}
+	if len(node.Children) > 0 {
+		for _, c := range node.Children {
+			dfs(c, post)
+		}
+	}
+	*post = append(*post, node.Val)
+}
+
+func postorder(root *Node) []int {
+	var post []int
+	dfs(root, &post)
+	return post
 }

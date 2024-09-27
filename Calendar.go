764b7934a1dc7e5af705b32e85e064ddf74fcdb7
Nguyen Sang Sinh <sangsinh789@gmail.com>
2024-09-27 15:27:31 +0700

    ajourter58(vendredi 27, septembre):731. my calendar II

--- Calendar.go
+++ Calendar.go
@@ -13,7 +13,11 @@ func Constructor() MyCalendar {
 	return MyCalendar{intervals: []Interval{}}
 }
 
+<<<<<<< HEAD
 func (c *MyCalendar) Book(start int, end int) bool {
+=======
+func (c *MyCalendar) BookConflict(start int, end int) bool {
+>>>>>>> 1e7fb06... ajourter58(vendredi 27, septembre):731. my calendar II
 	newInterval := Interval{Start: start, End: end}
 
 	for _, existing := range c.intervals {
@@ -25,3 +29,31 @@ func (c *MyCalendar) Book(start int, end int) bool {
 	c.intervals = append(c.intervals, newInterval)
 	return true
 }
+<<<<<<< HEAD
+=======
+
+func (c *MyCalendar) Book(start int, end int) bool {
+	newInterval := Interval{Start: start, End: end}
+
+	conflict := Constructor()
+	for _, existing := range c.intervals {
+		if !(newInterval.End <= existing.Start || newInterval.Start >= existing.End) {
+			startConflict := existing.Start
+			if newInterval.Start > existing.Start {
+				startConflict = newInterval.Start
+			}
+			endConflict := existing.End
+			if newInterval.End < existing.End {
+				endConflict = newInterval.End
+			}
+			flag := conflict.BookConflict(startConflict, endConflict)
+			if !flag {
+				return false
+			}
+		}
+	}
+
+	c.intervals = append(c.intervals, newInterval)
+	return true
+}
+>>>>>>> 1e7fb06... ajourter58(vendredi 27, septembre):731. my calendar II
--- main.go
+++ main.go
@@ -9,8 +9,16 @@ func main() {
 	var records []bool
 	calendar := Constructor()
 	records = append(records, calendar.Book(10, 20))
+<<<<<<< HEAD
 	records = append(records, calendar.Book(15, 25))
 	records = append(records, calendar.Book(20, 30))
+=======
+	records = append(records, calendar.Book(50, 60))
+	records = append(records, calendar.Book(10, 40))
+	records = append(records, calendar.Book(5, 15))
+	records = append(records, calendar.Book(5, 10))
+	records = append(records, calendar.Book(25, 55))
+>>>>>>> 1e7fb06... ajourter58(vendredi 27, septembre):731. my calendar II
 	fmt.Println(records)
 
 }

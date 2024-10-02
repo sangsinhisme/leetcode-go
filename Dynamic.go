a0a8d568ecb5a1f3341ce9293fbe92efb343c20f
Nguyen Sang Sinh <sangsinh789@gmail.com>
2024-10-02 15:25:41 +0700

    dynamicPrograming(mercredi, 2 octobre): #1. longest palindromic substring

--- Dynamic.go
+++ Dynamic.go
@@ -166,3 +166,35 @@ func minExtraChar(s string, dictionary []string) int {
 	}
 	return minimum
 }
+
+/*
+5. Longest Palindromic Substring
+https://leetcode.com/problems/longest-palindromic-substring/description
+*/
+func longestPalindrome(s string) string {
+	n := len(s)
+	dp := make([][]bool, n)
+	ans := []int{0, 0}
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+	for i := range dp {
+		dp[i][i] = true
+	}
+	for i := 0; i < n-1; i++ {
+		if s[i] == s[i+1] {
+			dp[i][i+1] = true
+			ans = []int{i, i + 1}
+		}
+	}
+	for diff := 2; diff < n; diff++ {
+		for i := 0; i < n-diff; i++ {
+			j := i + diff
+			if s[i] == s[j] && dp[i+1][j-1] {
+				dp[i][j] = true
+				ans = []int{i, j}
+			}
+		}
+	}
+	return s[ans[0] : ans[1]+1]
+}
